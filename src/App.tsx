7d7dc793c9d86da4ea21fc98ad437c4e05595a54
samuelthaiyil <sathaiyi@lakeheadu.ca>
2024-07-30 10:55:11 -0400

    added save button

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import {ProfilePicture} from "./components/ProfilePicture";
 
 function App() {
   return (
-      <>
-        <h1 className="text-3xl">Edit your Contact Information</h1>
-        <ProfilePicture />
-      </>
+      <div className="mx-auto w-1/2">
+          <div className="flex justify-between items-center my-5">
+              <h1 className="inline text-2xl ">Edit your Contact Information</h1>
+              <button className="text-sm bg-orange-500 font-semibold rounded text-white px-14 py-2 py-auto uppercase hover:bg-orange-400 transition hover:duration-100">Save</button>
+          </div>
+          <ProfilePicture />
+      </div>
   );
 }
 
