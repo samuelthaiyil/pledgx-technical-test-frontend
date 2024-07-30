6e914cedd807580f44ba5aec0d2ee6948131f0fd
samuelthaiyil <sathaiyi@lakeheadu.ca>
2024-07-30 09:49:36 -0400

    created app

---
+++ src/components/ProfilePicture.tsx
@@ -0,0 +1,10 @@
+export const ProfilePicture = () => {
+
+    return (
+        <>
+            <div className="">
+            </div>
+            <img src="Untitled_Artwork 16 copy 3 1.svg" alt="Profile picture"/>
+        </>
+    )
+}
