1774b31d0ff8122e9be26fc6fe51b76c7afe3d72
samuelthaiyil <sathaiyi@lakeheadu.ca>
2024-07-30 21:21:26 -0400

    added get user api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,9 +3,15 @@ import axios from "axios";
 
 const baseUrl = 'http://localhost:3000';
 
-export const submitForm = async (form: Form) => {
-    const url = `${baseUrl}/submit-form`;
+export const submitUser = async (form: Form) => {
+    const url = `${baseUrl}/submit-user`;
     return axios.post(url, {
         ...form
     });
 };
+
+export const getUser = async () => {
+    const url = `${baseUrl}/get-user`;
+    return axios.get(url);
+};
+
