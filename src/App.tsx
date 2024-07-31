b910975e23fbce0047aa3994aa2b763c4a316415
samuelthaiyil <sathaiyi@lakeheadu.ca>
2024-07-30 21:26:26 -0400

    updated responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {ProfilePicture} from "./components/ProfilePicture";
 import {FormField} from "./components/FormField";
 import {CountrySelector} from "./components/CountrySelector";
 import {Form} from "./models/Form";
-import {submitForm} from "./services/api";
+import {getUser, submitUser} from "./services/api";
 
 function App() {
     const [form, setForm] = useState<Form>({
@@ -12,24 +12,33 @@ function App() {
         lastName: "",
         phoneNumber: "",
         jobTitle: "",
-        country: ""
+        country: null
     });
 
-    const handleCountrySelected = (name: string, imageSrc: string): void => {
-        console.log(name, imageSrc)
-        setForm((form) => ({
-            ...form,
-            country: name
-        }));
-    }
+    useEffect( () => {
+        const getForm = async () => {
+            const user = await getUser();
+            const {first_name, last_name, phone_number, job_title, country} = user.data;
+
+            setForm((form) => ({
+                ...form,
+                firstName: first_name,
+                lastName: last_name,
+                phoneNumber: phone_number,
+                jobTitle: job_title,
+                country
+            }));
+        }
+        getForm();
+    }, [])
 
     const handleSubmit = async () => {
-        await submitForm(form);
+        await submitUser(form);
     }
 
   return (
-      <div className="mx-auto w-1/2 space-y-24">
-          <div className="flex justify-between items-center my-5">
+      <div className="mx-auto w-1/2 lg:w-1/3 space-y-24">
+          <div className="py-8 space-y-5 lg:flex lg:justify-between lg:items-center lg:my-5 lg:space-y-0">
               <h1 className="inline text-2xl ">Edit your Contact Information</h1>
               <button
                   className="text-sm bg-orange-500 font-semibold rounded text-white px-14 py-2 py-auto uppercase hover:bg-orange-400 transition hover:duration-100"
@@ -50,7 +59,7 @@ function App() {
               </div>
               <FormField onChange={(value) => setForm({...form, phoneNumber: value})} value={form.phoneNumber} label="Phone Number"/>
               <FormField onChange={(value) => setForm({...form, jobTitle: value})} value={form.jobTitle} label="Job Title"/>
-              <CountrySelector onSelect={handleCountrySelected} />
+              <CountrySelector selected={form.country} onSelect={(value) => setForm({...form, country: value})} />
           </div>
 
       </div>
